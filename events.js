f418c64d125371fb0c0fbf24d5f50022f6b0706a
josephsawaya <43706296+josephsawaya@users.noreply.github.com>
2019-06-01 22:02:09 -0400

    External JS file integration

---
+++ events.js
@@ -0,0 +1,1 @@
+var DATA= [{"lat" : "43.469761", "long" : "-80.538811","name":"Transportation 1","type":"transport","description":"Testing description #1"},{"lat" : "43.4696", "long" : "-80.5389","name":"Transportation name 2","type":"transport","description":"Testing description #2"}];
