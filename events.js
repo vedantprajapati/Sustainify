934ba262bab1224384d8333a980fce61e66b7bfe
JustinCK23 <45020085+JustinCK23@users.noreply.github.com>
2019-06-01 22:08:52 -0400

    Update events.js

--- events.js
+++ events.js
@@ -1,1 +1,16 @@
-var DATA= [{"lat" : "43.469761", "long" : "-80.538811","name":"Transportation 1","type":"transport","description":"Testing description #1"},{"lat" : "43.4696", "long" : "-80.5389","name":"Transportation name 2","type":"transport","description":"Testing description #2"}];
+var DATA = [
+  {
+    lat: "43.469761",
+    long: "-80.538811",
+    name: "Transportation 1",
+    type: "transport",
+    description: "Testing description #1"
+  },
+  {
+    lat: "43.4696",
+    long: "-80.5389",
+    name: "Transportation name 2",
+    type: "transport",
+    description: "Testing description #2"
+  }
+];
