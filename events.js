15b99bc241f66e874ac104f4ac309b8a3cceb63b
josephsawaya <43706296+josephsawaya@users.noreply.github.com>
2019-06-02 06:58:52 -0400

    Update events.js

--- events.js
+++ events.js
@@ -1,21 +1,21 @@
 var DATA = [
     { 
-    "lat": "43.469761",
-    "long": "-80.538811", 
+    "lat": " 43.470359",
+    "long": "-80.541254", 
     "name": "Transportation 1", 
     "type": "transport", 
     "description": "Testing description #1",
     "meetup":"STC" 
     },
-    { "lat": "43.4695", 
-    "long": "-80.5391", 
+    { "lat": "43.471822", 
+    "long": " -80.54384", 
     "name": "Food 1", 
     "type": "food", 
     "description": "Testing description #2",
     "meetup":"DC" 
     },
-    {"lat": "43.4700", 
-    "long": "-80.5400", 
+    {"lat": "43.471048 ", 
+    "long": "-80.547665", 
     "name": "Event 1", 
     "type": "event", 
     "description": "Testing description #2",
